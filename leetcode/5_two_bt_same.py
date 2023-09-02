2b9d15b17b42850f3db7d56ab4c23ea33c2bce9a
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-09-01 21:35:34 -0500

    Create 5_two_bt_same.py

---
+++ leetcode/5_two_bt_same.py
@@ -0,0 +1,26 @@
+# Link: https://leetcode.com/problems/same-tree/submissions/
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and q: return False
+        if not q and p: return False
+        if not p and not q: return True
+        
+        if p.val != q.val:
+            return False
+        is_left_same = self.isSameTree(p.left, q.left)
+        is_right_same = self.isSameTree(p.right, q.right)
+        if not is_left_same or not is_right_same:
+            return False
+        else:
+            return True
+
+
+# better sol: https://www.youtube.com/watch?v=BhuvF_-PWS0&list=PLJ_vPQ_vraNz90tiB1HNgUWjivW07RcXC&index=16
