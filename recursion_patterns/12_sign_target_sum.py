040243fda193cb6cc9b39da9f915de7afe5a8868
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-10-20 20:03:37 -0500

    Create 12_sign_target_sum.py

---
+++ recursion_patterns/12_sign_target_sum.py
@@ -0,0 +1,22 @@
+# Link: https://leetcode.com/problems/target-sum/
+
+# Solution (recursion)
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        def find(ind, curr, res):
+            if ind == n:
+                if curr == target:
+                    res[0] += 1
+                return
+            
+            # append a positive sign
+            find(ind+1, curr + nums[ind], res)
+            
+            # append a negative sign
+            find(ind+1, curr - nums[ind], res)
+        
+        n = len(nums)
+        res = [0]
+        find(0, 0, res)
+        return res[0]
