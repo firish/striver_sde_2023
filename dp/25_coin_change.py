10c90763593a20a6d614c173564a59086f95d550
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-10-28 02:29:11 -0500

    Create 25_coin_change.py

---
+++ dp/25_coin_change.py
@@ -0,0 +1,32 @@
+# Link: https://leetcode.com/problems/coin-change/submissions/
+
+
+# memo
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        def change(ind, curr, dp):
+            if ind >= len(coins):
+                return 10**12
+            if curr > amount:
+                return 10**12
+            if curr == amount:
+                return 0
+            
+            # memo check
+            if dp[ind][curr] != 10**12:
+                return dp[ind][curr]
+            
+            # keep taking current coin till you can
+            same = 1 + change(ind, curr+coins[ind], dp)
+            # move to the next coin
+            diff = 0 + change(ind+1, curr, dp)
+            
+            # memoize
+            dp[ind][curr] = min(same, diff)
+            return dp[ind][curr]
+        
+
+        dp = [[10**12 for _ in range(amount+1)] for _ in range(len(coins))]
+        res = change(0, 0, dp)
+        return res if res != 10**12 else -1
