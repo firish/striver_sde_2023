3779e8938a3b480ab1aac16093f01e6135a29d83
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-11-01 01:43:59 -0500

    Update 26_coin_change2.py

--- dp/26_coin_change2.py
+++ dp/26_coin_change2.py
@@ -24,3 +24,27 @@ class Solution:
         
         dp = [[-1 for _ in range(amount+1)] for _ in range(len(coins))]
         return coin_change(len(coins)-1, amount)
+
+
+# tabulation
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        dp = [[0 for _ in range(amount+1)] for _ in range(len(coins))]
+        # base case
+        for ind in range(len(coins)):
+            dp[ind][0] = 1
+        for amt in range(amount+1):
+            if amt % coins[0] == 0:
+                dp[0][amt] = 1
+        
+        # tabulation
+        for ind in range(1, len(coins)):
+            for amt in range(1, amount+1):
+                same, nxt = 0, 0
+                if amt >= coins[ind]:
+                    same = dp[ind][amt-coins[ind]]
+                nxt = dp[ind-1][amt]
+                dp[ind][amt] = same+nxt
+        
+        return dp[len(coins)-1][amount]
