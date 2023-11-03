62c6999997ba741e69302d3ce901116a5a1c6dff
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-11-03 16:29:36 -0500

    Update 27_LCS.py

--- dp/27_LCS.py
+++ dp/27_LCS.py
@@ -43,3 +43,17 @@ dp = [[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
                 else:
                     dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2], dp[ind1][ind2-1])
         return dp[len(text1)][len(text2)]
+
+
+# cleaner tabulation
+n1 = len(str1)
+n2 = len(str2)
+
+dp = [[0 for j in range(n2+1)] for i in range(n1+1)]
+
+for i in range(1,n1+1):
+    for j in range(1,n2+1):
+        if str1[i-1] == str2[j-1]:
+            dp[i][j] = 1+ dp[i-1][j-1]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
