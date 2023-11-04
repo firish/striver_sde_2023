98c7917f3d80eade82d3344b8d168e246ff41b58
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-11-04 06:38:26 -0500

    Update 34_print_LIS.py

--- dp/34_print_LIS.py
+++ dp/34_print_LIS.py
@@ -15,3 +15,33 @@ class Solution:
                 ans[lis-1] = nums[i]
                 lis -= 1
         print(ans)
+
+
+# A more stable way of printing the LIS
+from collections import deque
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        dp = [1] * len(nums)
+        mapping = [0] * len(nums)
+        maxi = 0
+        max_ind = 0
+        for ind in range(1, len(nums)):
+            mapping[ind] = ind
+            for prev in range(0, ind):
+                if nums[ind] > nums[prev]:
+                    if dp[ind] < 1 + dp[prev]:
+                        dp[ind] = 1 + dp[prev]
+                        mapping[ind] = prev
+            if dp[ind] > maxi:
+                maxi = dp[ind]
+                max_ind = ind
+        
+        # to print lis
+        ans = deque()
+        while mapping[max_ind] != max_ind:
+            ans.appendleft(nums[max_ind])
+            max_ind = mapping[max_ind]
+        ans.appendleft(nums[max_ind])
+        print(ans)
+        return maxi
