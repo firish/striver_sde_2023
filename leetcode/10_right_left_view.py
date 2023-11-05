3e0220c57e67f9a33309f7b5b6bd70ba27914d0f
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-11-05 16:48:59 -0600

    Create 10_right_left_view.py

---
+++ leetcode/10_right_left_view.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        
+        res = []
+        q = deque()
+        if root: q.append((root))
+
+        while q:
+            l = len(q)
+            for _ in range(l):
+                node = q.popleft()
+                
+                if node.left: 
+                    q.append(node.left)
+                if node.right: 
+                    q.append(node.right)
+            res.append(node.val)
+        
+        return res
+
+    def leftSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        
+        res = []
+        q = deque()
+        if root: q.append((root))
+
+        while q:
+            l = len(q)
+            first = True
+            for _ in range(l):
+                node = q.popleft()
+                if first:
+                  res.append(node.val)
+                  first = False
+                if node.left: 
+                    q.append(node.left)
+                if node.right: 
+                    q.append(node.right)
+        
+        return res
