858ccb56efed34eef0c436461af05e065020ab33
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-11-08 03:30:14 -0600

    Update 35_ways_to_score_points.py

--- dp/35_ways_to_score_points.py
+++ dp/35_ways_to_score_points.py
@@ -1,6 +1,29 @@
 points = [1, 2, 3, 4]
 n = len(points)
 calls = [0]
+def get_combs(ind, target, n, points, calls):
+    calls[0] += 1
+    # base case
+    if target < 0:
+        return 0
+    if ind == -1:
+        return 1 if target == 0 else 0
+        
+    # take the current number as many times as possible
+    take = get_combs(ind, target-points[ind], n, points, calls)
+    skip = get_combs(ind-1, target, n, points, calls)
+    return take + skip
+
+print(get_combs(n-1, 117, n, points, calls)*get_combs(n-1, 120, n, points, calls))
+print(calls)
+    
+# 170868600
+# [1696550]
+
+# LOOK AT THE DIFF WITH DP!!
+points = [1, 2, 3, 4]
+n = len(points)
+calls = [0]
 def get_combs(ind, target, n, points, dp, calls):
     calls[0] += 1
     # base case
@@ -25,3 +48,5 @@ dp2 = [[-1 for _ in range(121)] for _ in range(n)]
 print(get_combs(n-1, 117, n, points, dp1, calls)*get_combs(n-1, 120, n, points, dp2, calls))
 print(calls)
     
+# 170868600
+# [1542]
