8fd03237b4be7b47e83da9f83f59842055f372d0
Rishi Gulati <106293665+firish@users.noreply.github.com>
2023-11-13 02:16:28 -0600

    Create 1_dictionary.py

---
+++ design/1_dictionary.py
@@ -0,0 +1,13 @@
+# Designing a dictionary
+
+# Dict is nothing but a key-value pair. {"apple": 5}
+# Dict is used for O(1) lookup in a runtine.
+
+# Dict is also used in programming language's runtime. 
+# This is done in symbolic tables. If you have variables, or functions
+# The memory location of these variables, function, program routines is stored in a table called symbolic table
+# This table is nothing but a key-value store, or a dictionary.
+
+# Next, it is important to note that we can only store immutable data-types as dictionary keys, like int, str, or tuple
+# mutable data types like lists are not allowed. 
+
