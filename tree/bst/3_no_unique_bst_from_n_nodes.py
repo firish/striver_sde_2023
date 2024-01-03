b6718b9fb837ed16721f31670c4e38cedbf29d75
Rishi Gulati <106293665+firish@users.noreply.github.com>
2024-01-03 19:30:54 +0530

    Update 3_no_unique_bst_from_n_nodes.py

--- tree/bst/3_no_unique_bst_from_n_nodes.py
+++ tree/bst/3_no_unique_bst_from_n_nodes.py
@@ -23,3 +23,37 @@ class Solution:
         
         return get_subtrees(n)
         
+
+# Taking it a step further
+# DP
+class Solution:
+    def numTrees(self, n: int) -> int:
+        
+        def get_subtrees(target, dp):
+            # Base case
+            # No of subtrees with 0 and 1 node is 1
+            if target <= 1:
+                return 1
+            
+            # memo check
+            if dp[target] != -1:
+                return dp[target]
+            
+            # recurrence relation
+            # for getting the total BSTs
+            # Make each node the root
+            # all other nodes act as left and right children
+            # f(4) = f(0)*f(3) + f(1)*f(2) + f(2)*f(1) + f(3)*f(0) 
+            combinations = 0
+            for ind in range(0, target):
+                left_subtree = get_subtrees(ind, dp)
+                right_subtree = get_subtrees(target-ind-1, dp)
+                combinations += (left_subtree * right_subtree)
+            
+            # memoize
+            dp[target] = combinations
+            return dp[target]
+        
+        dp = [-1 for _ in range(n+1)]
+        return get_subtrees(n, dp)
+        
