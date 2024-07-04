a84aca9527b28a15834bda12aeef05c24097e018
Rishi Gulati <106293665+firish@users.noreply.github.com>
2024-07-04 09:31:26 +0530

    Update 5_create_dictionary.py

--- design/5_create_dictionary.py
+++ design/5_create_dictionary.py
@@ -1,17 +1,20 @@
 # Designing a dictionary
 
+####################################################### Introduction #######################################################
+# What is a dict?
 # Dict is nothing but a key-value pair. {"apple": 5}
 # Dict is used for O(1) lookup in a runtine.
+# Next, it is important to note that we can only store immutable data-types as dictionary keys, like int, str, or tuple
+# mutable data types like lists are not allowed. 
 
+# An interesting use case of dictionaries!
 # Dict is also used in programming language's runtime. 
 # This is done in symbolic tables. If you have variables, or functions
 # The memory location of these variables, function, program routines is stored in a table called symbolic table
 # This table is nothing but a key-value store, or a dictionary.
 
-# Next, it is important to note that we can only store immutable data-types as dictionary keys, like int, str, or tuple
-# mutable data types like lists are not allowed. 
 
-
+####################################################### Basic Working #######################################################
 # The basic working of a dict. (IMP)
 # Dictionaries are nothing but a class that uses the array data structure smartly.
 # Dictionaries work with a double hashing system.
