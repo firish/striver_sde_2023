d9b855db8211a585e0909d5cf37a58134745de0f
Rishi Gulati <106293665+firish@users.noreply.github.com>
2024-11-22 20:18:57 -0800

    Update 01_find_if_path_exists.py

--- 01_find_if_path_exists.py
+++ 01_find_if_path_exists.py
@@ -55,3 +55,61 @@ class Solution:
         # print(ds.rank)
         # print(ds.parent)
         return ds.are_connected(source, destination)
+
+
+# using Size
+class DisjointSet:
+
+    def __init__(self, n):
+        self.size = [1]*n
+        self.parent = [num for num in range(0, n)]
+
+    # To find the ultimae parent
+    def find_parent(self, node):
+        # base case
+        # when a node is its own parent
+        if node == self.parent[node]:
+            return node
+
+        # path compression
+        pn = self.find_parent(self.parent[node])
+        self.parent[node] = pn
+        
+        # return the parent
+        return pn
+
+
+    # Grow the disjoint set
+    def union(self, u, v):
+        # find the ultimate parents
+        pu = self.find_parent(u)
+        pv = self.find_parent(v)
+        
+        # If they are already in the same set, do nothing
+        if pu == pv:
+            return
+        
+        # Always connect the lower rank to the higher rank
+        if self.size[pu] >= self.size[pv]:
+            self.parent[pv] = pu
+            self.size[pu] += self.size[pv]
+        else:
+            self.parent[pu] = pv
+            self.size[pv] += self.size[pu]
+
+    # check if two nodes are joint or disjoint
+    def are_connected(self, u, v):
+        return self.find_parent(u) == self.find_parent(v)
+
+    
+class Solution:
+    
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # create the disjoint set
+        ds = DisjointSet(n)
+        for edge in edges:
+            ds.union(edge[0], edge[1])
+        
+        # print(ds.size)
+        # print(ds.parent)
+        return ds.are_connected(source, destination)
