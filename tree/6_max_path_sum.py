cb4bcdbcc4b94eb1b5b77516868897eb1928b31c
Rishi Gulati <106293665+firish@users.noreply.github.com>
2025-05-17 20:19:48 -0700

    Update 6_max_path_sum.py

--- tree/6_max_path_sum.py
+++ tree/6_max_path_sum.py
@@ -20,3 +20,33 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         self.get_sum(root)
         return self.maxi
+
+
+# 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
+        def get_path_sum(node, max_path_sum):
+            if not node:
+                return 0
+            
+            # travese
+            left_sum = max(get_path_sum(node.left, max_path_sum), 0)
+            right_sum = max(get_path_sum(node.right, max_path_sum), 0)
+
+            # calculate diameter
+            current_path_sum = left_sum + right_sum + node.val
+            max_path_sum[0] = max(max_path_sum[0], current_path_sum)
+
+            # calculate sum
+            return max(left_sum, right_sum) + node.val
+        
+        max_path_sum = [-10**10]
+        _ = get_path_sum(root, max_path_sum)
+        return max_path_sum[0]
