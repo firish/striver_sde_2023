442f104b02e815ee202b05e224824f34358f6ecc
Rishi Gulati <106293665+firish@users.noreply.github.com>
2025-05-17 19:44:48 -0700

    Update 4_BT_max_diameter.py

--- tree/4_BT_max_diameter.py
+++ tree/4_BT_max_diameter.py
@@ -24,4 +24,34 @@ class Solution:
         h= get_height(root, maxi)
         return maxi[0]
 
+
+# 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        def get_height(node, max_diameter):
+            if not node:
+                return 0
+            
+            # travese
+            left_height = get_height(node.left, max_diameter)
+            right_height = get_height(node.right, max_diameter)
+
+            # calculate diameter
+            max_diameter[0] = max(max_diameter[0], left_height + right_height)
+
+            # calculate height
+            return max(left_height, right_height) + 1
+        
+        max_diameter = [0]
+        max_height = get_height(root, max_diameter)
+        return max_diameter[0]
+
+
  
