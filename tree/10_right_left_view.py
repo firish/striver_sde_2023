b16196faf7575999b9dee7f29743835c30aaed55
Rishi Gulati <106293665+firish@users.noreply.github.com>
2025-05-18 15:43:39 -0700

    Update 10_right_left_view.py

--- tree/10_right_left_view.py
+++ tree/10_right_left_view.py
@@ -55,3 +55,36 @@ class Solution:
             q.append(node.right)
 # do the reverse, right first and then left, 
 # and then add the last node itself.
+
+
+
+# 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+
+        # write level order traversal
+        q = deque()
+        q.append([root])
+        res = [root.val]
+        while len(q) > 0:
+            curr = q.popleft()
+            level = []
+            for node in curr:
+                if node.left:
+                    level.append(node.left)
+                if node.right:
+                    level.append(node.right)
+            if len(level) > 0:
+                q.append(level)
+                res.append(level[-1].val)
+                # replace with res.append(level[0].val) for left view
+        return res
