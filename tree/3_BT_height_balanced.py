326cdc2441eb0bddcf92dc85a12c91e761b82d24
Rishi Gulati <106293665+firish@users.noreply.github.com>
2025-05-27 23:15:44 -0700

    Update 3_BT_height_balanced.py

--- tree/3_BT_height_balanced.py
+++ tree/3_BT_height_balanced.py
@@ -26,3 +26,31 @@ class Solution:
         res = check(root)
         if res == -1: return False
         return True
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def find_height(root):
+            if not root:
+                return 0
+
+            left = find_height(root.left)
+            if left == -1:
+                return -1
+            right = find_height(root.right)
+            if right == -1:
+                return -1
+                
+            if abs(left-right) > 1:
+                return -1
+
+            return max(left, right) + 1
+        
+        return find_height(root) != -1
