5c75a7fb4ec420f9ec708006eced2cb93b8cb829
Rishi Gulati <106293665+firish@users.noreply.github.com>
2025-06-03 23:31:24 -0700

    Update 8_boundary_traversal.py

--- tree/8_boundary_traversal.py
+++ tree/8_boundary_traversal.py
@@ -1,5 +1,78 @@
 # Python3 program for boundary traversal of binary tree
 
+# Actual solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        # Null case
+        if not root:
+            return []
+        
+        # helper to check if a node is a leaf 
+        def is_leaf(node):
+            if not node.left and not node.right:
+                return True
+            return False
+
+        # Head Reccursion - left boundary
+        def get_left(node, left):
+            if node.left and not is_leaf(node.left):
+                left.append(node.left.val)
+                get_left(node.left, left)
+            if not node.left and node.right and not is_leaf(node.right):
+                left.append(node.right.val)
+                get_left(node.right, left)
+        
+        # Tail Reccursion - right boundary - in reverse order
+        def get_right(node, right):
+            if node.right and not is_leaf(node.right):
+                get_right(node.right, right)
+                right.append(node.right.val)
+                
+            if not node.right and node.left and not is_leaf(node.left):
+                get_right(node.left, right)
+                right.append(node.left.val)
+                
+        # Head recursion - bottom boundary - left to right traversal
+        def get_bottom(node, bottom):
+            if is_leaf(node):
+                bottom.append(node.val)
+                return
+            
+            if node.left:
+                get_bottom(node.left, bottom)
+            if node.right:
+                get_bottom(node.right, bottom)
+
+        left = []
+        if root.left:
+            if not is_leaf(root.left):
+                left = [root.left.val]
+                get_left(root.left, left)
+        # print(left)
+
+        right = []
+        if root.right:
+            if not is_leaf(root.right):
+                get_right(root.right, right)
+                right.append(root.right.val)
+        # print(right)
+
+        bottom = []
+        if root and not is_leaf(root):
+            get_bottom(root, bottom)
+        # print(bottom)
+
+        return [root.val] + left + bottom + right
+
+
+
+# LOL
 class Node:
 	def __init__(self, data):
 		self.data = data 
