45444a8537b533908fa34da4be87fe1331829e0b
Rishi Gulati <106293665+firish@users.noreply.github.com>
2025-06-14 12:08:59 -0700

    Update 1_n-queens.py

--- recursion and backtracking/1_n-queens.py
+++ recursion and backtracking/1_n-queens.py
@@ -52,3 +52,69 @@ def n_queens(n, col, board):
             neg_diag[row-col] = False
 
 n_queens(N, 0, board)   
+
+
+
+
+
+### second pass
+import copy
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        # game board
+        board = [['.']*n for _ in range(n)]
+        queens = 0
+
+        # game state trackers
+        invalid_rows = set()
+        invalid_cols = set()
+        invalid_pos_diags = set() # r - c for all pts on pos diag is same
+        invalid_neg_diags = set() # r + c for all pts on neg diag is same
+
+        def check_position_validity(r, c):
+            # queen attack horizontal
+            if r in invalid_rows:
+                return False
+            # queen attack vertical
+            if c in invalid_cols:
+                return False
+            # queen attack diagonal
+            if r + c in invalid_neg_diags:
+                return False
+            if r - c in invalid_pos_diags:
+                return False
+            return True
+
+        res = []
+        def get_all_board_positions(c):
+            nonlocal board, queens, invalid_rows, invalid_cols, invalid_pos_diags, invalid_neg_diags
+
+            # end condition - successfully placed the queens
+            if c >= n-1 and queens == n:
+                res.append([''.join(r) for r in board])
+                return
+            
+            for r in range(0, n):
+                if check_position_validity(r, c):
+                    # place queen
+                    invalid_rows.add(r)
+                    invalid_cols.add(c)
+                    invalid_neg_diags.add(r+c)
+                    invalid_pos_diags.add(r-c)
+                    board[r][c] = 'Q'
+                    queens += 1
+                    # recurse
+                    get_all_board_positions(c+1)
+                    # backtrack
+                    invalid_rows.remove(r)
+                    invalid_cols.remove(c)
+                    invalid_neg_diags.remove(r+c)
+                    invalid_pos_diags.remove(r-c)
+                    board[r][c] = '.'
+                    queens -= 1
+                
+        
+        # start placement from 1st col
+        get_all_board_positions(0)
+
+        return res
